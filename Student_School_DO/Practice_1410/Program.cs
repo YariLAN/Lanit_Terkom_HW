381cb0f23d741b80499ec7b0964096dea0b3fba4
YariLAN <LordYarik22@mail.ru>
2023-10-14 14:02:13 +0300

    add Initial FooBar

--- Student_School_DO/Practice_1410/Program.cs
+++ Student_School_DO/Practice_1410/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(FooBar(15));
+            Console.WriteLine(FooBar(12));
+            Console.WriteLine(FooBar(25));
+        }
+
+        public static string FooBar(int num)
+        {
+            if (num % 5 == 0 && num % 3 == 0)
+            {
+                return "FooBar";
+            }
+            else if (num % 5 == 0 && num % 3 != 0)
+            {
+                return "Foo";
+            }
+            else if (num % 5 != 0 && num % 3 == 0)
+            {
+                return "Bar";
+            }
+            else
+                return "";
         }
     }
 }
