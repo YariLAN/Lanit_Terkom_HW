62c3754e861b969ec7a143a9af8aa758119577a9
YariLAN <LordYarik22@mail.ru>
2023-10-14 16:03:41 +0300

    add function RemakeArray

--- Student_School_DO/Practice_1410/Program.cs
+++ Student_School_DO/Practice_1410/Program.cs
@@ -4,27 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(FooBar(15));
-            Console.WriteLine(FooBar(12));
-            Console.WriteLine(FooBar(25));
+            var arr = new int[10][];
+
+            for (int y = 0; y < arr.Length; y++)
+            {
+                arr[y] = new int[10];
+                for (int x = 0; x < arr[y].Length; x++)
+                {
+                    arr[y][x] = new Random().Next(0, 4);
+                }
+            }
+
+            var s = RemakeArray(arr);
+
+            foreach (var line in arr)
+            {
+                foreach (var c in line)
+                    Console.Write(c + " ");
+                Console.WriteLine();
+            }
+
         }
 
-        public static string FooBar(int num)
+        static int[][] RemakeArray(int[][] array)
         {
-            if (num % 5 == 0 && num % 3 == 0)
+            foreach(int[] line in array)
             {
-                return "FooBar";
+                for(int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] != 0)
+                    {
+                        line[i] = 1;
+                    }
+                }
             }
-            else if (num % 5 == 0 && num % 3 != 0)
-            {
-                return "Foo";
-            }
-            else if (num % 5 != 0 && num % 3 == 0)
-            {
-                return "Bar";
-            }
-            else
-                return "";
+            return array;
         }
     }
 }
